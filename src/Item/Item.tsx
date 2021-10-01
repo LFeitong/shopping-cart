6b75bd763e69aaee9f5ac76725481a93c85a7df3
Feitong Liu <feitong@udel.edu>
2021-09-30 22:41:20 -0400

    style for item

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -1,13 +1,13 @@
 import Button from '@material-ui/core/Button';
 import { CartItemType } from '../App';
-
+import { Wrapper } from './Item.styles';
 type Props = {
   item: CartItemType;
   handleAddToCart: (clickedItem: CartItemType) => void;
 };
 
 const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-  <div>
+  <Wrapper>
     <img src={item.image} alt={item.title} />
     <div>
       <h3>{item.title}</h3>
@@ -15,7 +15,7 @@ const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
       <h3>${item.price}</h3>
     </div>
     <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
-  </div>
+  </Wrapper>
 );
 
 export default Item;
